52c7b93018840e52b2c1752b917ebef03366d5d6
Noah Morton <noahmorton98@gmail.com>
2017-02-18 12:14:42 -0600

    Added some user config vars, small tweaks

--- control.lua
+++ control.lua
@@ -1,11 +1,20 @@
+-- USER CONFIG VARS ---------
+
+-- Change this if you want the mod to take more belts for the same distance, for balancing
+local BELT_COST_MULTIPLIER = 1 --(DEFAULT (integer): 1)
+-- Change this to false if you'd like to not get the belts back when you break the pair of belts.
+local SHOULD_REFUND_BELTS = true -- (DEFAULT (boolean): true)
+
+-----------------------------
+
 script.on_event({defines.events.on_built_entity}, --run whenever the player builds an entity
    function(e)
       local entity=e.created_entity
-      local player=game.players[e.player_index]
 
       --first check to see if it's my belt or pipe being built
       if entity.name=='subterrainian-belt' or entity.name=='fast-subterrainian-belt' or entity.name=='express-subterrainian-belt' then
          if entity.belt_to_ground_type == 'output' then --If they're building the ending piece
+            local player=game.players[e.player_index]
             --get neighbor of belt and figure out distance
             local inputEntity = entity.neighbours[1] --the opening end of the pair of belts
             local IX = inputEntity.position.x
@@ -13,11 +22,11 @@ script.on_event({defines.events.on_built_entity}, --run whenever the player buil
             local OX = entity.position.x
             local OY = entity.position.y
             local distance = math.sqrt(math.pow(math.abs(math.floor(IX) - math.floor(OX)),2) + math.pow(math.abs(math.floor(IY) - math.floor(OY)),2))
-            --player.print("Placed output, distance is " .. distance)
 
             --check if they have the correct amount of the respective belt in their inventory
+            ------------------------NORMAL BELT
             if entity.name=='subterrainian-belt' then
-               if player.get_item_count("transport-belt") < distance then --if they don't have enough
+               if player.get_item_count("transport-belt") < (distance * BELT_COST_MULTIPLIER) then --if they don't have enough
                   entity.destroy() -- Destroy the entity
                   player.print("Not enough transport belts in inventory to create this length.")
 
@@ -28,12 +37,12 @@ script.on_event({defines.events.on_built_entity}, --run whenever the player buil
                      player.cursor_stack.count = player.cursor_stack.count + 1
                   end
                else --they have enough
-                  player.remove_item{name="transport-belt",count=distance} --deduct the required belts from their inventory
-                  player.print(distance .. " transport belts were used to create the subterrainian belt.")
+                  player.remove_item{name="transport-belt",count=distance * BELT_COST_MULTIPLIER} --deduct the required belts from their inventory
                end
 
+               ---------------------FAST BELT
             elseif entity.name == 'fast-subterrainian-belt' then
-               if player.get_item_count("fast-transport-belt") < distance then --if they don't have enough
+               if player.get_item_count("fast-transport-belt") < (distance * BELT_COST_MULTIPLIER) then --if they don't have enough
                   entity.destroy() -- Destroy the entity
                   player.print("Not enough fast transport belts in inventory to create this length.")
 
@@ -44,12 +53,12 @@ script.on_event({defines.events.on_built_entity}, --run whenever the player buil
                      player.cursor_stack.count = player.cursor_stack.count + 1
                   end
                else --they have enough
-                  player.remove_item{name="fast-transport-belt",count=distance} --deduct the required belts from their inventory
-                  player.print(distance .. " fast transport belts were used to create the subterrainian belt.")
+                  player.remove_item{name="fast-transport-belt",count=distance * BELT_COST_MULTIPLIER} --deduct the required belts from their inventory
                end
 
+               ------------------EXPRESS BELT
             elseif entity.name == 'express-subterrainian-belt' then
-               if player.get_item_count("express-transport-belt") < distance then --if they don't have enough
+               if player.get_item_count("express-transport-belt") < (distance * BELT_COST_MULTIPLIER)then --if they don't have enough
                   entity.destroy() -- Destroy the entity
                   player.print("Not enough express transport belts in inventory to create this length.")
 
@@ -60,15 +69,14 @@ script.on_event({defines.events.on_built_entity}, --run whenever the player buil
                      player.cursor_stack.count = player.cursor_stack.count + 1
                   end
                else --they have enough
-                  player.remove_item{name="express-transport-belt",count=distance} --deduct the required belts from their inventory
-                  player.print(distance .. " express transport belts were used to create the subterrainian belt.")
+                  player.remove_item{name="express-transport-belt",count=distance * BELT_COST_MULTIPLIER} --deduct the required belts from their inventory
                end
-
             end
-
          end
       elseif entity.name=='subterrainian-pipe' then
          --todo same as above
       end
    end --ends the function in the event
 )
+
+--todo refund belts when broken

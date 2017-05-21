44c2d7335a37080d0d69ec8aea5943b935b55e43
Noah Morton <noahmorton98@gmail.com>
2017-05-21 18:54:38 -0500

    Fixed graphics being broken for sub pipes

--- prototypes/entity.lua
+++ prototypes/entity.lua
@@ -209,82 +209,22 @@ data:extend({
 
 -- Pipe
 
-data:extend({
+local subPipe = table.deepcopy(data.raw["pipe-to-ground"]["pipe-to-ground"])
+subPipe.flags = {"placeable-neutral", "player-creation"}
+subPipe.name = "subterranean-pipe"
+subPipe.minable = {hardness = 0.2, mining_time = 2, result = "subterranean-pipe"}
+subPipe.max_distance = 250
+subPipe.max_health = 200
+subPipe.fluid_box.pipe_connections[2].max_underground_distance = 250
 
-      {
-         type = "pipe-to-ground",
-         name = "subterranean-pipe",
-         icon = "__base__/graphics/icons/pipe-to-ground.png",
-         flags = {"placeable-neutral", "player-creation","not-blueprintable","not-deconstructable"},
-         minable = {hardness = 0.2, mining_time= 2, result = "subterranean-pipe"},
-         max_health = 100,
-         max_distance = 250,
-         corpse = "small-remnants",
-         resistances =
-            {
-               {
-                  type = "fire",
-                  percent = 80
-               }
-            },
-         collision_box = {{-0.29, -0.29}, {0.29, 0.2}},
-         selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
-         fluid_box =
-            {
-               base_area = 1,
-               pipe_covers = pipecoverspictures(),
-               pipe_connections =
-                  {
-                     { position = {0, -1} },
-                     {
-                        position = {0, 1},
-                        max_underground_distance = 250
-                     }
-                  },
-            },
-         underground_sprite =
-            {
-               filename = "__core__/graphics/arrows/underground-lines.png",
-               priority = "extra-high-no-scale",
-               width = 64,
-               height = 64,
-               scale = 0.5
-            },
-         vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
-         pictures =
-            {
-               up =
-                  {
-                     filename = "__base__/graphics/entity/pipe-to-ground/pipe-to-ground-up.png",
-                     priority = "high",
-                     width = 44,
-                     tint = {r=1,g=0.5,b=0.1,a=1},
-                     height = 32 --, shift = {0.10, -0.04}
-                  },
-               down =
-                  {
-                     filename = "__base__/graphics/entity/pipe-to-ground/pipe-to-ground-down.png",
-                     priority = "high",
-                     width = 40,
-                     tint = {r=1,g=0.5,b=0.1,a=1},
-                     height = 32 --, shift = {0.05, 0}
-                  },
-               left =
-                  {
-                     filename = "__base__/graphics/entity/pipe-to-ground/pipe-to-ground-left.png",
-                     priority = "high",
-                     width = 32,
-                     tint = {r=1,g=0.5,b=0.1,a=1},
-                     height = 42 --, shift = {-0.12, 0.1}
-                  },
-               right =
-                  {
-                     filename = "__base__/graphics/entity/pipe-to-ground/pipe-to-ground-right.png",
-                     priority = "high",
-                     width = 32,
-                     tint = {r=1,g=0.5,b=0.1,a=1},
-                     height = 40 --, shift = {0.1, 0.1}
-                  },
-            }
-      }
-})
+subPipe.pictures.up.tint = {r=0.3,g=0,b=0}
+subPipe.pictures.left.tint = {r=0.3,g=0,b=0}
+subPipe.pictures.right.tint = {r=0.3,g=0,b=0}
+subPipe.pictures.down.tint = {r=0.3,g=0,b=0}
+
+subPipe.pictures.up.hr_version.tint = {r=0.3,g=0,b=0}
+subPipe.pictures.left.hr_version.tint = {r=0.3,g=0,b=0}
+subPipe.pictures.right.hr_version.tint = {r=0.3,g=0,b=0}
+subPipe.pictures.down.hr_version.tint = {r=0.3,g=0,b=0}
+
+data:extend{subPipe}

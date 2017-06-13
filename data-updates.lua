adc46f51c28614c03f0da3e98fb8ec2c03f81b37
Noah Morton <noahmorton98@gmail.com>
2017-06-13 10:47:30 -0500

    Added the ability for the mod to modify bob's logistics underground belts.

---
+++ data-updates.lua
@@ -0,0 +1,7 @@
+if data.raw["underground-belt"]["green-underground-belt"] then --bob's logistics is installed
+    data.raw["transport-belt"]["green-underground-belt"].max_distance = 250
+end
+
+if data.raw["underground-belt"]["purple-underground-belt"] then --bob's logistics is installed
+    data.raw["underground-belt"]["purple-underground-belt"].max_distance = 250
+end
